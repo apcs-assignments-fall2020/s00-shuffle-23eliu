bc90b2b3382f72d3d0b36d3e1e62907678e1838d
23eliu <23eliu@athenian.org>
2021-01-07 00:58:24 +0000

    finished both methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,37 +1,34 @@
+import java.lang.Math;
+
 public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) {
-        int arr1[] = new int[arr.length/2];
-        int arr2[] = new int[arr.length/2];
+        int arr1[] = {arr[0], arr[1], arr[2], arr[3], arr[4]};
+        int arr2[] = {arr[5], arr[6], arr[7], arr[8], arr[9]};
         int both[] = new int[arr.length];
         int num = 0;
-
-        for (int i = 0; i < arr.length/2; i++) {
-            arr1[num] = arr[i];
+        
+        for (int i = 0; i < arr1.length; i++) {
+            both[num] = arr1[i];
             num++;
-        }
-        num = 0;
-
-        for (int j = arr.length/2; j < arr.length/2; j++) {
-            arr2[num] = arr[j];
-            num++;
-        }
-        num = 0;
-
-        for (int k = 0; k < arr1.length; k++) {
-            both[num] = arr1[k];
-            num++;
-            both[num] = arr2[k];
+            both[num] = arr2[i];
             num++;
         }
 
-        return arr2;
+        return both;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
-    public static int[] selectionShuffle(int[] arr) { 
-        return null;
+    public static int[] selectionShuffle(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int rand = (int)(Math.random() * (arr.length - 1 + 1));
+            temp = arr[i];
+            arr[i] = arr[rand];
+            arr[rand] = temp;
+        }
+        return arr;
     }
 
 
