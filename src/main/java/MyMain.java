d018ebdc370e8678cfbeb54f0275e61b1c74fec3
23eliu <23eliu@athenian.org>
2021-01-07 00:11:21 +0000

    worked on perfect shuffle

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,8 +1,32 @@
 public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
-    public static int[] perfectShuffle(int[] arr) { 
-        return null;
+    public static int[] perfectShuffle(int[] arr) {
+        int arr1[] = new int[arr.length/2];
+        int arr2[] = new int[arr.length/2];
+        int both[] = new int[arr.length];
+        int num = 0;
+
+        for (int i = 0; i < arr.length/2; i++) {
+            arr1[num] = arr[i];
+            num++;
+        }
+        num = 0;
+
+        for (int j = arr.length/2; j < arr.length/2; j++) {
+            arr2[num] = arr[j];
+            num++;
+        }
+        num = 0;
+
+        for (int k = 0; k < arr1.length; k++) {
+            both[num] = arr1[k];
+            num++;
+            both[num] = arr2[k];
+            num++;
+        }
+
+        return arr2;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
